76c39efcf4cc46c98ff028c1f3fb0ab8c25bfee6
Helena Zhao <zhaohel@oregonstate.edu>
2024-07-29 20:42:16 -0700

    Wrote test1 to see if password too short is rejected correctly

--- check_pwd.py
+++ check_pwd.py
@@ -1,2 +1,2 @@
 def check_pwd(pwd):
-    return False
+    return True
--- tests.py
+++ tests.py
@@ -6,15 +6,6 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertFalse(check_pwd("Boba@12"))  # 7 characters
 
-    def test2(self):
-        self.assertFalse(check_pwd("Boba@1111112345687890"))  # 21 characters
-
-    def test3(self):
-        self.assertFalse(check_pwd("boba@123"))  # contain 1 lowercase characters
-
-    def test4(self):
-        self.assertFalse(check_pwd("Boba@123"))  # contain everything valid
-
 if __name__ == '__main__':
     unittest.main()
 
