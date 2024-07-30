3da9a55db04ae3023e7bab8e586daeb828f72e0a
Helena Zhao <zhaohel@oregonstate.edu>
2024-07-29 20:27:20 -0700

    Wrote test3 to check if having a lower case character passes

--- tests.py
+++ tests.py
@@ -5,8 +5,12 @@ class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertFalse(check_pwd("Boba@12"))  # 7 characters
+
     def test2(self):
         self.assertFalse(check_pwd("Boba@1111112345687890"))  # 21 characters
+
+    def test3(self):
+        self.assertFalse(check_pwd("boba@123"))  # contain 1 lowercase characters
 
 if __name__ == '__main__':
     unittest.main()
