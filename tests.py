48a2ea44b90111e852adfce7b1936285fceba957
Helena Zhao <zhaohel@oregonstate.edu>
2024-07-29 20:19:46 -0700

    Wrote test2 to see if too many characters is rejected correctly

--- tests.py
+++ tests.py
@@ -4,8 +4,9 @@ from check_pwd import check_pwd
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        self.assertFalse(check_pwd("Boba@12"))  # 7 characters, should be False
-
+        self.assertFalse(check_pwd("Boba@12"))  # 7 characters
+    def test2(self):
+        self.assertFalse(check_pwd("Boba@1111112345687890"))  # 21 characters
 
 if __name__ == '__main__':
     unittest.main()
